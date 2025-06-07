9025d1a4bd762f0bbac4db7860f27ce30319c459
pxkundu <pxkundu2@shockers.wichita.edu>
2025-06-07 18:13:16 -0400

    Initial nextjs skalaton for Amplify deployment

--- web/amplify/functions/list-sensors/resource.ts
+++
@@ -1,6 +0,0 @@
-import { defineFunction } from "@aws-amplify/backend";
-
-export const listSensors = defineFunction({
-  entry: "./handler.ts",
-  name: "listSensors",
-});
--- web/amplify/functions/send-sensor-value/resource.ts
+++
@@ -1,7 +0,0 @@
-import { defineFunction } from "@aws-amplify/backend";
-
-export const sendSensorValue = defineFunction({
-  entry: "./handler.ts",
-  name: "sendSensorValue",
-  resourceGroupName: "data",
-});
